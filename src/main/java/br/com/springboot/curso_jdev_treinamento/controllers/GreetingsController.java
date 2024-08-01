227988c504289f414e9657c9a9132762030326e4
Alex Castro <160438618+AnyTimeR@users.noreply.github.com>
2024-08-01 19:03:43 -0300

    get, post and delete

--- src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -1,10 +1,19 @@
 package br.com.springboot.curso_jdev_treinamento.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,9 +47,33 @@ public class GreetingsController {
 
         Usuario usuario = new Usuario();
         usuario.setNome(nome);
-        usuarioRepository.save(usuario);
+        usuarioRepository.save(usuario); /* grava no banco de dados */
 
         return "Ola mundo " + nome;
+    }
+
+    @GetMapping(value = "listatodos") /* nosso primeiro método de API */
+    @ResponseBody /* retorna os dados para o corpo da resposta */
+    public ResponseEntity<List<Usuario>> listaUsuario() {
+        List<Usuario> usuarios = usuarioRepository.findAll(); /* executa a consulta no banco de dados */
+
+        return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK); /* retorna a lista em JSON */
+    }
+
+    @PostMapping(value = "salvar") /* mapeia a url */
+    @ResponseBody /* descrição da resposta */
+    public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario) { /* recebe os dados para salvar */
+        Usuario user = usuarioRepository.save(usuario);
+
+        return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(value = "delete") /* mapeia a url */
+    @ResponseBody /* descrição da resposta */
+    public ResponseEntity<String> delete(@RequestParam Long iduser) { /* recebe os dados para salvar */
+        usuarioRepository.deleteById(iduser);
+
+        return new ResponseEntity<String>("user deletado com sucesso", HttpStatus.OK);
     }
 
 }
