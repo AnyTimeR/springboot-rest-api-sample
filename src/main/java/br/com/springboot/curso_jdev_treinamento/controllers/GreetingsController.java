7c3d001f590992b2db7fe4708b482ce167fb9424
AnyTimeR <amdcastro17@gmail.com>
2024-08-03 18:41:44 -0300

    busca por id e nome

--- src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -5,12 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -68,12 +69,40 @@ public class GreetingsController {
         return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
     }
 
+    @PutMapping(value = "atualizar") /* mapeia a url */
+    @ResponseBody /* descrição da resposta */
+    public ResponseEntity<?> atualizar(@RequestBody Usuario usuario) { /* recebe os dados para salvar */
+        if(usuario.getId() == null) {
+            return new ResponseEntity<String>("id não foi informado para atualização", HttpStatus.OK);
+        }
+        
+        Usuario user = usuarioRepository.saveAndFlush(usuario);
+
+        return new ResponseEntity<Usuario>(user, HttpStatus.OK);
+    }
+
     @DeleteMapping(value = "delete") /* mapeia a url */
     @ResponseBody /* descrição da resposta */
-    public ResponseEntity<String> delete(@RequestParam Long iduser) { /* recebe os dados para salvar */
+    public ResponseEntity<String> delete(@RequestParam Long iduser) { /* recebe os dados para deletar */
         usuarioRepository.deleteById(iduser);
 
         return new ResponseEntity<String>("user deletado com sucesso", HttpStatus.OK);
+    }
+
+    @GetMapping(value = "buscaruserid") /* mapeia a url */
+    @ResponseBody /* descrição da resposta */
+    public ResponseEntity<Usuario> buscaruserid(@RequestParam(name = "iduser") Long iduser) { /* recebe os dados para consultar */
+        Usuario usuario = usuarioRepository.findById(iduser).get();
+
+        return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "buscarPorNome") /* mapeia a url */
+    @ResponseBody /* descrição da resposta */
+    public ResponseEntity<List<Usuario>> buscarPorNome(@RequestParam(name = "name") String name) { /* recebe os dados para consultar */
+        List<Usuario> usuario = usuarioRepository.buscarPorNome(name.trim().toUpperCase());
+
+        return new ResponseEntity<List<Usuario>>(usuario, HttpStatus.OK);
     }
 
 }
